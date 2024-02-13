a6a424da90ca15fcf3c726a41d80f4e18027d584
Mikhail Demichev <demichev.mi@gmail.com>
2024-02-13 15:17:57 +0700

    Initial Commit

---
+++ DietDuo/DietDuoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DietDuoApp.swift
+//  DietDuo
+//
+//  Created by Mikhail Demichev on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DietDuoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
